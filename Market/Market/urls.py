55906de6644fd78a174c2dbbc83c50aa293036c3
EdoBrm <edo.brm@gmail.com>
2023-05-25 19:29:19 +0300

    added orders

--- Market/Market/urls.py
+++ Market/Market/urls.py
@@ -7,5 +7,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('order/', include('order.urls')),
     path('cart/', include('cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
